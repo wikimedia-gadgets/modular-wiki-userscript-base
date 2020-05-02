769ebd4807794c96573cd5dc25fbf2f834beb7a9
Evad37 <evad37@gmail.com>
2020-05-02 23:51:05 +0800

    Remove deprecated ResourceLoader module

--- myscriptname-src/index.js
+++ myscriptname-src/index.js
@@ -2,7 +2,7 @@ import App from './modules/App';
 
 Promise.all([
 	// Resource loader modules
-	mw.loader.using(['mediawiki.util', 'mediawiki.api', 'mediawiki.Title', 'mediawiki.RegExp']),
+	mw.loader.using(['mediawiki.util', 'mediawiki.api', 'mediawiki.Title']),
 	// Page ready
 	$.ready
 ]).then(function() {
