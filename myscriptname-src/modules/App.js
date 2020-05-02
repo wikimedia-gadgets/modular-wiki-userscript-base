d47168c91064f76caf2efb7f181d3e1fc982e8fa
Evad37 <evad37@gmail.com>
2020-05-03 00:06:23 +0800

    Use es6 syntax

--- myscriptname-src/modules/App.js
+++ myscriptname-src/modules/App.js
@@ -2,11 +2,11 @@ import hello from './hello';
 
 const App = () => {
     // Example: Using a custom module (imported above)
-    var helloTo = "me";
-    var greeting1 = hello(helloTo);
+    const helloTo = "me";
+    const greeting1 = hello(helloTo);
     console.log("greeting1", greeting1);
-    var greeting2 = hello();
-    console.log("greeting2", greeting2);
+    const greeting2 = hello();
+    console.log({greeting2});
     
     // Example: Using a MedaiWiki ResourceLoader module (loaded in index.js)
     var api = new mw.Api();
@@ -18,7 +18,7 @@ const App = () => {
 		rvprop: ['user', 'content'],
 		rvlimit: 1,
 		rvdir: 'newer'
-	} ).then(function(response) {
+	} ).then(response => {
         console.log("response", response);
     });
 }
